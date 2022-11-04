64de2de59b3a1c6c889d1ca5a48aefee82a8a42f
Mensch <mensch272@users.noreply.github.com>
2022-11-04 11:44:45 +0500

    feat: different impl for different types.

--- glue_derive/src/lib.rs
+++ glue_derive/src/lib.rs
@@ -1,9 +1,16 @@
-use proc_macro2::TokenStream;
-use quote::{__private::Span, quote};
+mod args;
+
+use quote::quote;
 use syn::{
-    parenthesized, parse::Parse, parse_macro_input, punctuated::Punctuated, token::Comma, Block,
-    FnArg, Ident, PatType, Path, ReturnType, Token, TypePath,
+    parenthesized, parse::Parse, parse_macro_input, punctuated::Punctuated, Block, FnArg, Ident,
+    Path, ReturnType, Token, TypePath,
 };
+
+use crate::args::{get_extern_params, get_extern_params_stream};
+
+const SUPPORTED_TYPES: [&'static str; 10] = [
+    "u8", "u16", "u32", "u64", "i8", "i16", "i32", "i64", "f32", "f64",
+];
 
 struct Expose {
     name: Ident,
@@ -58,33 +65,40 @@ pub fn expose(
         .map(|params| {
             params
                 .iter()
-                .map(|param| {
-                    let return_ident = &param.pat.pat;
-                    let return_ty = &param.pat.ty;
-                    let ptr_ident = &param.ident;
+                .map(|arg| {
+                    let pat = &arg.pat.pat;
+                    let ty = &arg.pat.ty;
 
                     macro_rules! match_rtype {
-                        ($($ty:ident => $ac:block),+, _ => $el:block) => {
-                            match return_ty.as_ref() {
+                        ($($ty:expr => $ac:block),+, _ => $el:block) => {
+                            match ty.as_ref() {
                                 $(
-                                    syn::Type::Path(TypePath {qself: _, path: Path { leading_colon: _, segments }}) if segments.last().unwrap().ident == "$ty" => $ac
+                                    syn::Type::Path(TypePath {qself: _, path: Path { leading_colon: _, segments }}) if $ty.contains(&segments.last().unwrap().ident.to_string().as_str()) => $ac
                                 ),*
                                 syn::Type::Path(TypePath {qself: _, path: _}) => $el,
-                                _ => panic!("'{}' is not supported in exposed function", quote!(#return_ty)),
+                                _ => panic!("'{}' is not supported in exposed function", quote!(#ty)),
                             }
                         };
                     }
 
                     match_rtype! {
-                        String => {
+                        SUPPORTED_TYPES => {
+                            quote! {}
+                        },
+                        ["bool"] => {
                             quote! {
-                                let #return_ident: #return_ty = unsafe { std::ffi::CString::from_raw(#ptr_ident).into_string().unwrap() };
+                                let #pat: #ty = #pat != 0;
+                            }
+                        },
+                        ["String"] => {
+                            quote! {
+                                let #pat: #ty = unsafe { std::ffi::CString::from_raw(#pat).into_string().unwrap() };
                             }
                         },
                         _ => {
                             quote! {
-                                let #return_ident: #return_ty = unsafe {
-                                    let __c = std::ffi::CString::from_raw(#ptr_ident).into_string().unwrap();
+                                let #pat: #ty = unsafe {
+                                    let __c = std::ffi::CString::from_raw(#pat).into_string().unwrap();
                                     serde_json::from_str(&__c).unwrap()
                                 };
                             }
@@ -139,37 +153,4 @@ pub fn expose(
 
     println!("{expanded}");
     expanded.into()
-}
-
-/// Minimal struct to hold extern function arg
-struct ExternArg {
-    ident: Ident,
-    ty: TokenStream,
-    pat: PatType,
-}
-
-fn get_extern_params(params: &Punctuated<FnArg, Comma>) -> Vec<ExternArg> {
-    params
-        .iter()
-        .map(|arg| match arg {
-            FnArg::Receiver(_) => panic!("'self' is not supported in exposed functions"),
-            FnArg::Typed(p) => p,
-        })
-        .enumerate()
-        .map(|(i, p)| ExternArg {
-            ident: Ident::new(&format!("ptr{i}"), Span::call_site()),
-            ty: quote!(*mut std::os::raw::c_char),
-            pat: p.clone(),
-        })
-        .collect::<Vec<_>>()
-}
-
-fn get_extern_params_stream(params: &Vec<ExternArg>) -> Punctuated<TokenStream, Comma> {
-    params
-        .iter()
-        .map(|arg| {
-            let ExternArg { ident, ty, pat: _ } = arg;
-            quote!(#ident: #ty)
-        })
-        .collect()
 }
