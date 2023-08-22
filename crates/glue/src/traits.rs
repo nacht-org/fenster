7c0d7fdf36373a4a2fd02c1a77e2e78c41374d9b
Mensch <mensch272@users.noreply.github.com>
2023-08-22 19:27:52 +0500

    feat(docs): added documentation to quelle_glue::traits

--- crates/glue/src/traits.rs
+++ crates/glue/src/traits.rs
@@ -1,10 +1,27 @@
 use quelle_core::prelude::*;
 
+/// This is the minimal functionality required for each extension/source.
+///
+/// The trait should be exposed to wasm abi using [`expose_basic`]
+///
+/// ## Example
+///
+/// ```ignore
+/// struct ExtensionName;
+/// expose_basic!(ExtensionName);
+/// ```
 pub trait FetchBasic {
+    /// Retrieve the meta information about a novel and its chapter list.
     fn fetch_novel(url: String) -> Result<Novel, QuelleError>;
+
+    /// Fetch the content of the chapter as html text
+    ///
+    /// The returned html should be cleaned of all unnecessary content and tags.
+    /// This includes empty elements, ad elements, and most tags.
     fn fetch_chapter_content(url: String) -> Result<Content, QuelleError>;
 }
 
+/// The macro used to export [FetchBasic] to wasm abi
 #[macro_export]
 macro_rules! expose_basic {
     ($name:ident) => {
@@ -20,11 +37,25 @@ macro_rules! expose_basic {
     };
 }
 
+/// This trait adds popular search functionality to an extension/source
+///
+/// The trait should be exposed to wasm abi using [`expose_popular`]
+///
+/// ## Example
+///
+/// ```ignore
+/// struct ExtensionName;
+/// expose_popular!(ExtensionName);
+/// ```
 pub trait PopularSearch {
+    /// Construct a url pointing to the browseable popular page
     fn popular_url(page: i32) -> String;
+
+    /// Search for the popular/trending novels
     fn popular(page: i32) -> Result<Vec<BasicNovel>, QuelleError>;
 }
 
+/// The macro used to export [PopularSearch] to wasm abi
 #[macro_export]
 macro_rules! expose_popular {
     ($name:ident) => {
@@ -40,11 +71,33 @@ macro_rules! expose_popular {
     };
 }
 
+/// This trait adds text search functionality to an extension/source
+///
+/// The trait should be exposed to wasm abi using [`expose_text`]
+///
+/// ## Example
+///
+/// ```ignore
+/// struct ExtensionName;
+/// expose_text!(ExtensionName);
+/// ```
 pub trait TextSearch {
+    /// Construct a url pointing to a webpage with the given arguments
+    ///
+    /// In the case where a website url cannot be constructed with query
+    /// (ex: the novels are fetched with an api call), the next
+    /// closest webpage should be returned.
+    /// This can be the search page (ex: https://example.com/search) or if
+    /// no such page exists base url may be returned.
     fn text_search_url(query: String, page: i32) -> Result<String, QuelleError>;
+
+    /// Search for novels with the given query and page
+    ///
+    /// See [FilterSearch::filter_search] for more complex filtering implementations
     fn text_search(query: String, page: i32) -> Result<Vec<BasicNovel>, QuelleError>;
 }
 
+/// The macro used to export [TextSearch] to wasm abi
 #[macro_export]
 macro_rules! expose_text {
     ($name:ident) => {
@@ -55,22 +108,62 @@ macro_rules! expose_text {
     };
 }
 
+/// This trait adds filter search functionality to an extension/source
+///
+/// The trait should be exposed to wasm abi using [`expose_filter`]
+///
+/// ## Example
+///
+/// ```ignore
+/// struct ExtensionName;
+/// expose_filter!(ExtensionName);
+/// ```
 pub trait FilterSearch
 where
     <Self as FilterSearch>::Options: quelle_core::filter::InputField,
 {
+    /// The options that are available for filtering
+    ///
+    /// [FilterSearch] constrains the options to implement [InputField](quelle_core::filter::InputField).
+    /// This provides validation and filter parsing.
+    /// [InputField] can be derived given when all its field are also [InputField].
+    ///
+    /// ## Example
+    ///
+    /// ```rust
+    /// # use quelle_core::filter::{InputField, TextField};
+    /// # use quelle_glue_derive::InputField;
+    ///
+    /// #[derive(InputField)]
+    /// struct FilterOptions {
+    ///     title: TextField,
+    /// }
+    /// ```
     type Options;
+
+    /// A getter for [`Self::Options`] available for filtering
     fn filter_options() -> &'static Self::Options;
+
+    /// Construct a url pointing to a webpage with the given arguments
+    ///
+    /// In the case where a website url cannot be constructed with the filter
+    /// options (ex: the novels are fetched with an api call), the next
+    /// closest webpage should be returned.
+    /// This can be the search page (ex: https://example.com/search) or if
+    /// no such page exists base url may be returned.
     fn filter_search_url(
         filter: <Self::Options as InputField>::Type,
         page: i32,
     ) -> Result<String, QuelleError>;
+
+    /// Search for novels with the given filter and page
     fn filter_search(
         filter: <Self::Options as InputField>::Type,
         page: i32,
     ) -> Result<Vec<BasicNovel>, QuelleError>;
 }
 
+/// The macro used to export [FilterSearch] to wasm abi
 #[macro_export]
 macro_rules! expose_filter {
     ($name:ident) => {
