a3833e8c2d707a2a9aa52a1d424e84471fa4b0e8
Mensch <mensch272@users.noreply.github.com>
2023-08-25 11:49:51 +0500

    docs: added documentation setup_default and init_extension

--- crates/glue/src/setup.rs
+++ crates/glue/src/setup.rs
@@ -5,6 +5,12 @@ use crate::{
     prelude::{set_panic_hook, FromWasmAbi},
 };
 
+/// The default setup function exported
+///
+/// This setups the panic hook in debug mode
+/// and applies the config
+///
+/// See [init_extension]
 #[no_mangle]
 pub fn setup_default(config: *mut u8) {
     #[cfg(debug_assertions)]
@@ -14,6 +20,7 @@ pub fn setup_default(config: *mut u8) {
     init_extension(&config);
 }
 
+/// Initiate the extension with the config
 pub fn init_extension(config: &ExtensionConfig) {
     Logger::new(config.level_filter).init();
 }
